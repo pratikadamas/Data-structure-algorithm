1ec871171ece45693f2940f7d23de389dce7c919
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2025-04-11 01:02:53 +0530

    selection sort

--- selection sort.c
+++ selection sort.c
@@ -1,31 +1,44 @@
-//
-// Created by Pratik Giri on 05-04-2025.
-//
 #include <stdio.h>
 
-int main() {
-    printf("Hello World!\n");
-    int arr[4]={2,1,10,0} ;
-    int n=sizeof (arr)/sizeof (arr[0]);
-    printf("%d\n",n);
-    for(int i=0;i<n;i++){
-        int min=i;
-        for (int j = i+1; j <n ; ++j) {
-            if(arr[min]>arr[j]){
-                min=j;
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n ; i++) {
+        int min = i; // Assume current i is the minimum
 
+        // Find index of the minimum element in the unsorted part
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[min]) {
+                min = j;
             }
         }
-        int temp=arr[i];
-        arr[i]=arr[min];
-        arr[min]=temp;
 
+        // Swap the found minimum element with the first unsorted element
+        if (min != i) {
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
     }
+}
 
-    for (int i = 0; i <n ; ++i) {
-        printf("%d\n",arr[i]);
+// Function to print the array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
+    printf("\n");
+}
 
+int main() {
+    int arr[] = {2, 1, 10, 0};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array:\n");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    printf("Sorted array:\n");
+    printArray(arr, n);
 
     return 0;
 }
